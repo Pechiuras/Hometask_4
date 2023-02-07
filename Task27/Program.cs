05e911181ef572e987f46f1a89e6b60d429315df
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-07 14:11:44 +0500

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Task 27
+Console.Clear();
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while (n > 0)
+{
+result = result + n % 10; 
+n = n / 10; 
+}
+Console.WriteLine(result);
