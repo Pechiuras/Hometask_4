e92ae62458e6a9580ad6e1e837d13c452577a7cf
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-07 14:12:15 +0500

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,8 @@
+﻿//Task29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран(ввод пользователя с клавиатуры).
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 100);
+Console.Write($"Результат: [{string.Join(" , " , array)}]");
